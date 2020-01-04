0b8177820d6dcd7a8f5f2104242bb0e73c5437c1
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2020-01-04 09:20:41 -0600

    get cook county map files

---
+++ code/get_cook_county_block_code_map.py
@@ -0,0 +1,33 @@
+
+import json
+from pathlib import Path
+
+project_dir = str(Path(__file__).resolve().parents[1])
+print(f"running: {__file__}")
+
+
+# first, load census block code map for illinois
+file_path = project_dir + "/data/interem/" + "tl_2018_17_tabblock10.geojson"
+assert file_path ==  "/Users/erik/broadband_access_research/broadband-map-experiment/data/interem/tl_2018_17_tabblock10.geojson", "check file path"
+with open(file_path) as json_file:
+    map_il = json.load(json_file)
+print('tl_2018_17_tabblock10.geojson loaded as map_il')
+
+
+# remove non-cook county entries from the features list in map_il
+features = map_il['features']
+features_length_before = len(features)
+for i, feature in enumerate(features):
+    if feature['properties']['COUNTYFP10'] != "031":
+        del features[i]
+print('removed non-cook county entries from map_il')
+features_length_after = len(features)
+assert features_length_after < features_length_before, "features list should be shorter after removing non-cook-county entries"
+
+
+# write to json
+file_path = project_dir + "/data/interem/" + "cook_county_map_data.geojson"
+assert file_path == "/Users/erik/broadband_access_research/broadband-map-experiment/data/interem/cook_county_map_data.geojson", "check file path"
+with open(file_path, 'w') as outfile:
+    json.dump(map_il, outfile)
+print("writing map_il to cook_county_map_data.geojson done")
